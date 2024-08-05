874b4f310850b04d4eac2b9c1417279223174faa
Lab Luppes <labs@luppes.com>
2024-08-05 15:01:09 -0500

    Add DB Copy Example

---
+++ src/sql.database/sql.database/Patch/DbCopy-20240805.sql
@@ -0,0 +1,9 @@
+﻿PRINT 'Running: USE master'
+USE master
+GO
+
+PRINT 'Running: CREATE DATABASE Dadabase0805 AS COPY OF Dadabase'
+CREATE DATABASE Dadabase0805 AS COPY OF Dadabase
+GO
+
+Print 'Done!'
