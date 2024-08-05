228be0b75640be8864f96b53987634391299eb7b
Lab Luppes <labs@luppes.com>
2024-08-05 15:15:13 -0500

    Add DB Copy Example

--- src/sql.database/sql.database/Patch/DbCopy-20240805.sql
+++ src/sql.database/sql.database/Patch/DbCopy-20240805.sql
@@ -2,8 +2,20 @@
 USE master
 GO
 
-PRINT 'Running: CREATE DATABASE Dadabase0805 AS COPY OF Dadabase'
-CREATE DATABASE Dadabase0805 AS COPY OF Dadabase
+PRINT '--------------------------------------------------'
+PRINT 'Running: SELECT name FROM master.dbo.sysdatabases'
+SELECT name FROM master.dbo.sysdatabases
 GO
 
+PRINT '--------------------------------------------------'
+PRINT 'Running: CREATE DATABASE DadabaseV2 AS COPY OF Dadabase'
+CREATE DATABASE DadabaseV2 AS COPY OF Dadabase
+GO
+
+PRINT '--------------------------------------------------'
+PRINT 'Running: SELECT name FROM master.dbo.sysdatabases'
+SELECT name FROM master.dbo.sysdatabases
+GO
+
+PRINT '--------------------------------------------------'
 Print 'Done!'
